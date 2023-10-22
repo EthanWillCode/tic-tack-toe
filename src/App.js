1d84866a7a722c02cef81795410dbb5bcfd292a8
EthanWillCode <Aethil@yahoo.com>
2023-10-22 16:53:13 -0500

    moved state to game function.

--- src/App.js
+++ src/App.js
@@ -8,22 +8,40 @@ function Square({ value, onSquareClick }) {
   );
 }
 
-export default function Board() {
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+export default function Game (){
+  const [xIsNext, setXIsNext] =useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length-1];
 
+function handlePlay(nextSquares){
+  setHistory([...history, nextSquares]);
+  setXIsNext(!xIsNext);
+}
+
+  return (
+    <div className="game">
+      <div className="board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+        <ol>{/*todo*/}</ol>
+      </div>
+    </div>
+  );
+}
+
+function Board({xIsNext, squares, onPlay}) {
   function handleClick(i) {
     if(squares[i] || calculateWinner(squares)){
       return;
     }
     const nextSquares = squares.slice();
-    if(xIsNext === true){
+    if(xIsNext){
       nextSquares[i] = "X";
     } else {
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+    onPlay(nextSquares);
   }
 
   const winner = calculateWinner(squares);
